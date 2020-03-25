d162c0c28a6ade856fd13fcec66c89764a31d1e8
Eli Ben-Porat <eli.bp@jottabyte.io>
2020-03-25 10:43:36 -0400

    Cleanup old files

--- draft.rs
+++
@@ -1,4 +0,0 @@
-/// Module for grabbing draft data, with signing bonus data directly from the API for 2017 and onwards.
-/// For older seasons, we'll need to grab those from various parts of the internet
-/// 2016 data with bonuses: https://www.mlb.com/news/2016-draft-signing-and-bonus-tracker-c183906680
-/// 2015 data with bonuses: https://www.mlb.com/news/c-130246508
